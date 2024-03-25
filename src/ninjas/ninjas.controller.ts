a1ec3dd8c4b60bf886352df2c1f6e94a98413af4
Yankhoba <bagayoko.yan@gmail.com>
2024-03-25 09:53:13 +0100

    🚧 query() for ninjas

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+  // Get /ninjas --> []
+  @Get()
+  getNinjas(@Query('type') type: string) {
+    return [{ type }];
+  }
+
+  // Get /ninjas/:id --> {}
+  @Get(':id')
+  getOneNinja(@Param('id') id: string) {
+    return {
+      id,
+    };
+  }
+
+  @Post()
+  createNinjas(@Body() createNinjaDto) {
+    return {
+      name: createNinjaDto.name,
+    };
+  }
+
+  @Put(':id')
+  updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
+    return {
+      id,
+      name: updateNinjaDto,
+    };
+  }
+
+  @Delete(':id')
+  removeNinja(@Param('id') id: string) {
+    return {
+      id,
+    };
+  }
+}
